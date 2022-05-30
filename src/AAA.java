14b9c6ffb22a55f6a16ca22cf40c03fbe68f7360
teacher <teacher@lsby.local>
2022-05-30 17:32:06 +0300

    First commit

---
+++ src/AAA.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+public class AAA 
+{
+	
+	public static void main(String[] args) 
+	{
+		Scanner s=new Scanner(System.in);
+		/*
+		System.out.println("Hello girls!!! Good luck!!");
+		System.out.print("Enter two numbers: ");
+		int a, b;
+		a=s.nextInt();
+		b=s.nextInt();
+		System.out.println(a+b);
+		System.out.println("dfgdG"+(a+b));
+		double c,d;
+		c=s.nextDouble();
+		d=s.nextDouble();
+		System.out.println("Enter string: ");
+		String str;
+		s.nextLine();
+		str=s.nextLine();
+		System.out.println("The value of str is:  "+ str);
+	
+		
+		
+		String str;
+		//str=s.nextLine();
+		//char ch=str.toCharArray()[0];
+		int x=s.nextInt();
+		System.out.println(x);
+		s.nextLine();
+		x=s.nextInt();
+		System.out.println(x);
+		for(int i=0; i<5; i++)
+		{
+			str=s.nextLine();
+			System.out.println(str+"*");
+		}
+		*/
+		
+		int x=s.nextInt();
+		int[][] arr=new int[2][x];
+		
+		Random r=new Random();
+		x=r.nextInt(6)+20;
+
+	}
+
+}
